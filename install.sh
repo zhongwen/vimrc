1fed299b9793983f5b0b689930e0c0b9591ccd7c
Zhongwen Xu <zhongwen.s.xu@gmail.com>
2016-07-29 12:24:12 +1000

    submodules lastest version

--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
 mv ../vimrc ../.vim
 ln -s ~/.vim/vimrc ~/.vimrc
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git submodule update --init
